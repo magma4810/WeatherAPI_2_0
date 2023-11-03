71c7f7d1316c255f04bfaa47b99794890a10284a
Pavel <magma4810@gmail.com>
2023-11-03 22:29:49 +0300

    soon

--- weather.js
+++ weather.js
@@ -6,14 +6,37 @@
         return result.json();
     }
     const list = document.querySelector('#list');
-    async function addText(city){
-        const weather = await getWeather(city);
-        console.log(weather.main.temp)
-        list.innerHTML = `<ol>${`<li>${weather.main.temp}</li>`}</ol>`;
+    console.log(list)
+    function showWeather(){
+        list.innerHTML = `<ol>${items.map((el)=>`<li>${el}</li>`).join('')}</ol>`;
+        addToLocalStorage(items); 
+    }
+    function showText(items){
+        // const weather = await getWeather(city);
+        list.innerHTML = `<ol>${items.map((el)=>`<li>${el}</li>`).join('')}</ol>`;
+        // return weather
+    }
+    const storageKey = 'items';
+    function readLocalStorage(list){
+        try{
+            const data = localStorage.getItem(storageKey);
+            const items = data === null ? [] : JSON.parse(data);
+            console.log(items,list)
+            list.innerHTML = `<ol>${items.map((el)=>`<li>${el}</li>`).join('')}</ol>`;
+        }catch(e){
+            console.error(e);
+            return [];
+        }
+    }
+    readLocalStorage(list);
+    async function addToLocalStorage(items,value){
+        const weather = await getWeather(value);
+        items.push(weather.main.temp);
+        localStorage.setItem(storageKey,JSON.stringify(items));//??
     }
     const form = document.querySelector('form');
     form.addEventListener('submit',(ev)=>{
-
+        
         ev.preventDefault();
 
         const formElement = ev.target;
@@ -21,7 +44,9 @@
         const input = formElement.querySelector('input');
         const value = input.value;
         input.value = '';
+        console.log(items)
+        addToLocalStorage(items,value);
+        readLocalStorage(list);
         
-        addText(value)
-    })
+    });
 })();
